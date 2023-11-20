f94d2472efeca796ea21b6843e9e0de4755b2d01
Ryosuke-Mac <nakayamaryousuke@localhost.local>
2023-11-21 01:00:23 +0900

    udpate

--- src/main.c
+++ src/main.c
@@ -5,20 +5,23 @@ void
 fizzbuzz(int n, FILE *stream) 
 {
     // 無効な引数チェック
-    if (n <= 0) {
+    if (n <= 0) 
+    {
         fprintf(stderr, "fizzbuzz: 無効な引数です。\n");
         exit(EXIT_FAILURE);
     }
 
     // メモリ割り当てチェック
-    char *results = (char *)malloc(n * sizeof(char));
-    if (results == NULL) {
+    char *results = (char *)malloc(n * sizeof(results));
+    if (results == NULL) 
+    {
         fprintf(stderr, "fizzbuzz: メモリの割り当てに失敗しました。\n");
         exit(EXIT_FAILURE);
     }
 
     // FizzBuzzの計算
-    for (int i = 1; i <= n; i++) {
+    for (int i = 1; i <= n; i++) 
+    {
         if (i % 15 == 0) results[i-1] = 'F'; // FizzBuzz
         else if (i % 3 == 0) results[i-1] = 'f'; // Fizz
         else if (i % 5 == 0) results[i-1] = 'b'; // Buzz
@@ -26,7 +29,8 @@ fizzbuzz(int n, FILE *stream)
     }
 
     // streamがNULLでない場合のみ出力する。
-    if (stream != NULL) {
+    if (stream != NULL) 
+    {
         for (int i = 0; i < n; i++) {
             fprintf(stream, "%c", results[i]);
         }
@@ -41,21 +45,29 @@ int
 testFizzBuzz() 
 {
     FILE *devNull = fopen("/dev/null", "w");
-    if (devNull == NULL) {
+    if (devNull == NULL) 
+    {
         fprintf(stderr, "一時ファイルのオープンに失敗しました。\n");
-        return 0; // テスト失敗
+        return EXIT_FAILURE; // テスト失敗
     }
 
     fizzbuzz(15, devNull);
 
-    fclose(devNull);
+    if (fclose(devNull)) 
+    {
+	    perror("fclose");
+	    fprintf(stderr, "fcloseに失敗しました。");
+	    return EXIT_FAILURE;
+    }
+
     return 1; // テスト成功
 }
 
 int 
 main() 
 {
-    if (!testFizzBuzz()) {
+    if (!testFizzBuzz()) 
+    {
         fprintf(stderr, "テストが失敗しました。プロセスを中断します。\n");
         exit(EXIT_FAILURE);
     }
